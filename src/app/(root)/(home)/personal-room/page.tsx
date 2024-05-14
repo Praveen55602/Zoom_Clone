5e5d49dd85c8a2251abcfe1eb53068d76b1d1716
Praveen <praveensingh55602@gmail.com>
2024-05-15 00:03:56 +0530

    side bar and assets added

--- src/app/(root)/(home)/personal-room/page.tsx
+++ src/app/(root)/(home)/personal-room/page.tsx
@@ -1,11 +1,9 @@
 import React from "react";
 
-const Home = () => {
+export default function PersonalRoom() {
   return (
     <section className="flex size-full flex-col gap-10 text-white">
-      <h1 className="text-3xl font-bold">Home</h1>
+      <h1 className="text-3xl font-bold">Personal Room</h1>
     </section>
   );
-};
-
-export default Home;
+}
