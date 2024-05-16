5e80ade0587fccdb0171d293f9d50a1f5f2e7264
Praveen <praveensingh55602@gmail.com>
2024-05-17 00:24:07 +0530

    meeting setup component completed

---
+++ src/components/MeetingRoom.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const MeetingRoom = () => {
+  return (
+    <section className="relative h-screen w-full overflow-hidden pt-4 text-white">
+      <div className="relative flex size-full items-center justify-center">
+        <div className="flex size-full max-w-[1000px]"></div>
+      </div>
+    </section>
+  );
+};
+
+export default MeetingRoom;
